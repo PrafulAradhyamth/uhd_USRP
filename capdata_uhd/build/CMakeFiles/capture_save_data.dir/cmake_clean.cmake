3636a858d81ff1ba7784e3332ae06297cfa28075
Praful Aradhyamath <aradhyamathpraful@gamil.com>
2025-07-16 19:09:15 +0200

    added cmake

---
+++ capdata_uhd/build/CMakeFiles/capture_save_data.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/capture_save_data.dir/capture_save_data.cpp.o"
+  "CMakeFiles/capture_save_data.dir/capture_save_data.cpp.o.d"
+  "out/capture_save_data"
+  "out/capture_save_data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/capture_save_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ capdata_uhd/build/CMakeFiles/capture_save_data.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for capture_save_data.
+# This may be replaced when dependencies are built.
